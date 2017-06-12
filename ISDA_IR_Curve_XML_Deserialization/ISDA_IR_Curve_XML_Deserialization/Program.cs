78bfd17f5003883096c323426091b3ef427d3be3
kst_nmvk <cnaum@gmail.com>
2017-06-12 17:03:19 +0200

    First and last commit.

---
+++ ISDA_IR_Curve_XML_Deserialization/ISDA_IR_Curve_XML_Deserialization/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+using System.Xml.Serialization;
+
+
+namespace ISDA_IR_Curve_XML_Deserialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string XMLFilesDirectory = @"path to the XML file"; // replace this by the path to the file InterestRates_USD_20170609.xml
+            string Currency = @"USD";
+            string Date = "20170609";
+            string GenericFileName = "InterestRates_" + Currency + "_" + Date;
+            string XMLFileName = GenericFileName + ".xml";
+            string CurrencyXMLFileInitialLocationName = XMLFilesDirectory + @"\" + XMLFileName;
+            XmlSerializer serializer = new XmlSerializer(typeof(interestRateCurve));
+            Stream reader = new FileStream(CurrencyXMLFileInitialLocationName, FileMode.Open);
+            interestRateCurve IRCurve = (interestRateCurve)serializer.Deserialize(reader);
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+This project describes how to simply deserialize an XML provided by markit for an ISDA interest rates curve (for a given currency and at the given spot date) into an interest rates curve object.
+
+As you see the repo has (and will forever have) only one commit. If out of pure luck you manage to succeed in contacting me regarding this repository, I will not answer.
